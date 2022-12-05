ac84de774bc19815b63afa73053fad1d5e0834e5
vugonz <82618983+vugonz@users.noreply.github.com>
2022-12-05 15:51:49 +0000

    tweak to script

--- makediskread.sh
+++ makediskread.sh
@@ -6,11 +6,9 @@ if [ $# -lt 1 ]; then
     exit
 fi
 
-
+# config
 input_file="input.txt"
 
-echo "$1"/aoc.py
 sed -i "/requests/s/^/#/" "$1"/aoc.py
 sed -e "/raw_data = requests.*/a \    with open(\"$input_file\", 'r') as file:\n        raw_data = file.read()\n" -i "$1"/aoc.py
 sed "/raw_data.text/s/text.//" -i "$1"/aoc.py
-echo "$1"/aoc.py
